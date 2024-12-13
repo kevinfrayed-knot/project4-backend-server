43a95e81c1604785ddbdd28f669c00a8bd1cfeda
kevinfrayed-knot <kevin@frayed-knot.com>
2024-12-13 15:27:12 -0600

    updated models/User.js and models/Category.js

--- models/Category.js
+++ models/Category.js
@@ -1,11 +1,12 @@
+
+
+// backend/models/Category.js
 const mongoose = require('mongoose');
 
 const categorySchema = new mongoose.Schema({
   name: {
     type: String,
     required: true,
-    unique: true,
-    trim: true,
   },
 });
 
